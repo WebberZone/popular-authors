9368ee58b00c00a718f819a30b5c5689fa50b9b5
Ajay DSouza <me@ajaydsouza.com>
2020-05-24 16:54:29 +0100

    Initial commit

---
+++ popular-authors.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Popular Authors.
+ *
+ * Show a list of the popular authors using the widget or shortcode. A Top 10 WordPress plugin addon.
+ *
+ * @package   Popular_Authors
+ * @author    Ajay D'Souza
+ * @license   GPL-2.0+
+ * @link      https://webberzone.com
+ * @copyright 2020 WebberZone
+ *
+ * @wordpress-plugin
+ * Plugin Name: Popular Authors
+ * Plugin URI:  https://webberzone.com/downlods/popular-authors/
+ * Description: Show a list of the popular authors using the widget or shortcode. A Top 10 WordPress plugin addon.
+ * Version:     1.0.0-beta1
+ * Author:      Ajay D'Souza
+ * Author URI:  https://webberzone.com
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: top-10-fast-tracker
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Holds the filesystem directory path (with trailing slash) for Top 10
+ *
+ * @since 1.0.0
+ *
+ * @var string Plugin folder path
+ */
+if ( ! defined( 'POP_AUTH_PLUGIN_DIR' ) ) {
+	define( 'POP_AUTH_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+/**
+ * Holds the filesystem directory path (with trailing slash) for Top 10
+ *
+ * @since 1.0.0
+ *
+ * @var string Plugin folder URL
+ */
+if ( ! defined( 'POP_AUTH_PLUGIN_URL' ) ) {
+	define( 'POP_AUTH_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+/**
+ * Holds the filesystem directory path (with trailing slash) for Top 10
+ *
+ * @since 1.0.0
+ *
+ * @var string Plugin Root File
+ */
+if ( ! defined( 'POP_AUTH_PLUGIN_FILE' ) ) {
+	define( 'POP_AUTH_PLUGIN_FILE', __FILE__ );
+}
+
+
+/*
+ *---------------------------------------------------------------------------*
+ * Modules
+ *---------------------------------------------------------------------------*
+ */
+
+require_once POP_AUTH_PLUGIN_DIR . 'includes/main.php';
+require_once POP_AUTH_PLUGIN_DIR . 'includes/shortcode.php';
