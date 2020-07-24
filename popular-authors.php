1aed093b66a56620d42f2e4423863690d1cdadf3
Ajay DSouza <me@ajaydsouza.com>
2020-07-24 19:55:50 +0100

    Update popular-authors.php

--- popular-authors.php
+++ popular-authors.php
@@ -14,7 +14,7 @@
  * Plugin Name: Popular Authors
  * Plugin URI:  https://webberzone.com/downlods/popular-authors/
  * Description: Show a list of the popular authors using the widget or shortcode. A Top 10 WordPress plugin addon.
- * Version:     1.0.0
+ * Version:     1.0.1-beta1
  * Author:      Ajay D'Souza
  * Author URI:  https://webberzone.com
  * License:     GPL-2.0+
